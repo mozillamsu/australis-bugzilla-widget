e8b61fbaaa6cd72f886fa96ff224894265b42e74
Eric Slenk <slenkeri@msu.edu>
2013-09-21 00:17:32 -0400

    Converted user object to revealing module pattern.

--- data/bug-list.js
+++ data/bug-list.js
@@ -1,17 +1,64 @@
 /* ****
  * User
  * ****/
-var user_name = document.getElementById("user-name");
-var save_button = document.getElementById("save");
+var user = (function () {
+    var pub = {};
 
-save_button.addEventListener('click', function (event) {
-    user_name.value = "Saved";
-});
+    var formNameElement = document.getElementById("user-name");
+    pub.formElement = document.getElementById("user-input");
+
+    pub.onUserNameSubmit = function () {
+        var userName = getUserName();
+        var userNameFound = queryUserName(userName);
+        if (userNameFound) {
+            setUserName(userName);
+        } else {
+            rejectUserName();
+        }
+    };
+
+    function getUserName () {
+        return "Super Awesome User Name!";
+    };
+
+    function queryUserName (userName) {
+        return true;
+    };
+
+    function setUserName (userName) {
+        console.log("setUserName "+userName);
+    };
+
+    function rejectUserName () {
+        console.log("rejectUserName");
+    };
+
+    return pub;
+}());
+
+
+/* ********
+ * Bug List
+ * ********/
+/*
+var bugList = (function () {
+    var pub = {};
+
+    pub.BugList(element) {
+    }
+
+    function getUserBugs () {
+    }
+
+    return pub;
+}());
+*/
 
 
 /* ***************
- * Signal Handling
+ * Event Handling
  * ***************/
 self.port.on("show", function onShow() {
-    user_name.focus();
 });
+
+user.formElement.addEventListener('submit', user.onUserNameSubmit);
