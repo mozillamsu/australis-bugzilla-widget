fbef95b1e27fc8ac37c15d81e534f1d5a40e28cb
Eric Slenk <slenkeri@msu.edu>
2013-10-02 22:57:45 -0400

    Bug list can now be instantiated with query parameters and a category.

--- data/bug-list.js
+++ data/bug-list.js
@@ -1,11 +1,33 @@
-var bugList = (function () {
-    var pub = {};
+var BugList = function (categoryName, queryParameters) {
+    this.client = bz.createClient({
+        url: "https://api-dev.bugzilla.mozilla.org/latest",
+        timeout: 30000
+    });
+    this.categoryName = categoryName;
+    this.queryParameters = queryParameters;
+    this.queryBugs();
+}
 
-    pub.BugList(element) {
+BugList.prototype = {
+
+    queryBugs: function () {
+        this.client.searchBugs(this.queryParameters, function (error, bugs) {
+            if (error == true) {
+                console.log(error);
+            }
+            else {
+                console.log("Received "+bugs.length+" bugs.");
+                bugs.forEach(function(bug) {
+                    console.log(bug);
+                });
+            }
+        });
     }
 
-    function getUserBugs () {
-    }
+}
 
-    return pub;
-}());
+/*
+exports.init = function(categoryName, queryParameters) {
+    return new BugList (categoryName, queryParameters);
+}
+*/
